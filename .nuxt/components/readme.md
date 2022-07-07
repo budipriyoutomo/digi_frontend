1a9b572979c4b52cda7750e9d6f4859b812b9f93
Budi Priyo Utomo <utomo2@gmail.com>
2022-07-07 14:27:34 +0700

    first commit

---
+++ .nuxt/components/readme.md
@@ -0,0 +1,14 @@
+# Discovered Components
+
+This is an auto-generated list of components discovered by [nuxt/components](https://github.com/nuxt/components).
+
+You can directly use them in pages and other components without the need to import them.
+
+**Tip:** If a component is conditionally rendered with `v-if` and is big, it is better to use `Lazy` or `lazy-` prefix to lazy load.
+
+- `<CreateArticle>` | `<create-article>` (components/CreateArticle.vue)
+- `<NuxtLogo>` | `<nuxt-logo>` (components/NuxtLogo.vue)
+- `<TheFooter>` | `<the-footer>` (components/TheFooter.vue)
+- `<TheHeader>` | `<the-header>` (components/TheHeader.vue)
+- `<TheHero>` | `<the-hero>` (components/TheHero.vue)
+- `<TheModal>` | `<the-modal>` (components/TheModal.vue)
---
+++ .nuxt/router.js
@@ -0,0 +1,58 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { normalizeURL, decode } from 'ufo'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _1a60e196 = () => interopDefault(import('..\\pages\\article.vue' /* webpackChunkName: "pages/article" */))
+const _68528a0d = () => interopDefault(import('..\\pages\\create.vue' /* webpackChunkName: "pages/create" */))
+const _7ff5ca5e = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
+
+const emptyFn = () => {}
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: '/',
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+    path: "/article",
+    component: _1a60e196,
+    name: "article"
+  }, {
+    path: "/create",
+    component: _68528a0d,
+    name: "create"
+  }, {
+    path: "/",
+    component: _7ff5ca5e,
+    name: "index"
+  }],
+
+  fallback: false
+}
+
+export function createRouter (ssrContext, config) {
+  const base = (config._app && config._app.basePath) || routerOptions.base
+  const router = new Router({ ...routerOptions, base  })
+
+  // TODO: remove in Nuxt 3
+  const originalPush = router.push
+  router.push = function push (location, onComplete = emptyFn, onAbort) {
+    return originalPush.call(this, location, onComplete, onAbort)
+  }
+
+  const resolve = router.resolve.bind(router)
+  router.resolve = (to, current, append) => {
+    if (typeof to === 'string') {
+      to = normalizeURL(to)
+    }
+    return resolve(to, current, append)
+  }
+
+  return router
+}
